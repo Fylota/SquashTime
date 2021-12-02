97c6859a68314da2083fdd93bf4eb59368920c21
Fylota <fulop.agota01@gmail.com>
2021-12-02 17:56:22 +0100

    Added Navigation Drawer

--- app/src/main/java/hu/bme/aut/android/squashtime/HomeActivity.kt
+++ app/src/main/java/hu/bme/aut/android/squashtime/HomeActivity.kt
@@ -1,10 +1,7 @@
 package hu.bme.aut.android.squashtime
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.Menu
-import android.view.MenuItem
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -14,38 +11,47 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
-import com.google.firebase.auth.FirebaseAuth
-import hu.bme.aut.android.squashtime.databinding.ActivityPostsBinding
+import hu.bme.aut.android.squashtime.databinding.ActivityHomeBinding
 
-class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class HomeActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityPostsBinding
+    private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityPostsBinding.inflate(LayoutInflater.from(this))
-        setContentView(binding.root)
-        setSupportActionBar(binding.appBarPosts.toolbar)
 
-        binding.appBarPosts.fab.setOnClickListener { view ->
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarHome.toolbar)
+
+        binding.appBarHome.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-        binding.navView.setNavigationItemSelectedListener(this)
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_home)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.nav_logout -> {
-                FirebaseAuth.getInstance().signOut()
-                startActivity(Intent(this, MainActivity::class.java))
-                finish()
-            }
-        }
-
-        binding.drawerLayout.closeDrawer(GravityCompat.START)
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.home, menu)
         return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_home)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
